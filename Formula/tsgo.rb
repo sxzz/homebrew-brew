bece6fb8937f23aa785fedf900d7816c8a94e320
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-12 03:37:54 +0800

    feat: add livecheck for tsgo

--- Formula/tsgo.rb
+++ Formula/tsgo.rb
@@ -3,18 +3,27 @@ class Tsgo < Formula
   homepage "https://github.com/microsoft/typescript-go"
   license "Apache-2.0"
 
+  livecheck do
+    url "https://github.com/sxzz/tsgo-releases/releases/latest"
+    regex(/^build-(\d{4}-\d{2}-\d{2})$/i)
+    strategy :github_latest
+  end
+
   on_macos do
     on_arm do
       url "https://github.com/sxzz/tsgo-releases/releases/latest/download/tsgo-macos-arm64.tar.gz"
+      sha256 :no_check
     end
     on_intel do
       url "https://github.com/sxzz/tsgo-releases/releases/latest/download/tsgo-macos-amd64.tar.gz"
+      sha256 :no_check
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/sxzz/tsgo-releases/releases/latest/download/tsgo-linux-amd64.tar.gz"
+      sha256 :no_check
     end
   end
 
