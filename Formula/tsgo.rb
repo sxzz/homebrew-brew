da87b75c749b45d8d34b3b5568e3a5b8f913f334
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-03-12 02:58:42 +0800

    feat: add tsgo

---
+++ Formula/tsgo.rb
@@ -0,0 +1,30 @@
+class Tsgo < Formula
+  desc "Staging repo for development of native port of TypeScript"
+  homepage "https://github.com/microsoft/typescript-go"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/sxzz/tsgo-releases/releases/latest/download/tsgo-macos-arm64.tar.gz"
+    end
+    on_intel do
+      url "https://github.com/sxzz/tsgo-releases/releases/latest/download/tsgo-macos-amd64.tar.gz"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/sxzz/tsgo-releases/releases/latest/download/tsgo-linux-amd64.tar.gz"
+    end
+  end
+
+  def install
+    libexec.install Dir["*"]
+    chmod 0755, libexec/"tsgo"
+    bin.install_symlink libexec/"tsgo"
+  end
+
+  test do
+    system "#{bin}/tsgo", "-h"
+  end
+end
